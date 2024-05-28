5eec0b285f8b0ea416c576a05d585151bbcdde3c
Łukasz Biały <lukasz.marcin.bialy@gmail.com>
2024-05-28 11:45:22 +0200

    beautify auto api use

--- app/auto.scala
+++ app/auto.scala
@@ -8,12 +8,10 @@ class Auto(val pulumiAccessToken: String):
   def deploy(subdomain: String, content: String)(using cfg: Config): Either[Throwable, String] =
     val stackName = FullyQualifiedStackName("lbialy", "scalify", s"$subdomain-prod")
 
-    try
-      logout().orThrow
-
-      login(LoginOption.PulumiAccessToken(pulumiAccessToken)).orThrow
-
-      val workspace = localWorkspace(
+    for
+      _ <- logout()
+      _ <- login(LoginOption.PulumiAccessToken(pulumiAccessToken))
+      workspace <- localWorkspace(
         LocalWorkspaceOption.Repo(
           GitRepo(
             url = "https://github.com/lbialy/scali.fyi.git",
@@ -32,9 +30,8 @@ class Auto(val pulumiAccessToken: String):
           shell.pulumi.env.PulumiConfigPassphraseEnv -> "pass",
           "BESOM_LANGHOST_SCALA_CLI_OPTS" -> "--server=false"
         )
-      ).orThrow
-
-      val stack = upsertStackRemoteSource(
+      )
+      stack <- upsertStackRemoteSource(
         stackName,
         GitRepo(
           url = "https://github.com/lbialy/scali.fyi.git",
@@ -49,24 +46,14 @@ class Auto(val pulumiAccessToken: String):
           )
         ),
         LocalWorkspaceOption.EnvVars(shell.pulumi.env.PulumiConfigPassphraseEnv -> "pass")
-      ).orThrow
-
-      val config = Map[String, ConfigValue](
+      )
+      config = Map[String, ConfigValue](
         "cloudflare_token" -> ConfigValue(cfg.cloudflareToken, secret = true),
         "cloudflare_zone_id" -> ConfigValue(cfg.cloudflareZoneId),
         "site_content" -> ConfigValue(content),
         "site_subdomain" -> ConfigValue(subdomain)
       )
-
-      stack.setAllConfig(config).orThrow
-
-      val upRes = stack.up().orThrow
-      println(upRes)
-
-      Right(subdomain)
-    catch
-      case e: Throwable => Left(e)
-
-      // val stack = Stack(stackName)
-      // stack.create(content)
-      // Right(stackName.value)
+      _ <- stack.setAllConfig(config)
+      upRes <- stack.up()
+      _ = println(upRes)
+    yield subdomain
